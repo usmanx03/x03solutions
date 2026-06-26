1863cb7c2c9fe1527e739942a3621637e71fa5fd
Usman Zahid <developerusman@yahoo.com>
2026-06-26 19:04:07 +0500

    Design interation 1

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,24 @@
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
+import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default defineConfig({
-    base: "./",
-    plugins: [
-        tailwindcss(),
-    ],
+    base: './',
+    plugins: [tailwindcss()],
     build: {
         outDir: 'docs',
         emptyOutDir: true,
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                solutions: resolve(__dirname, 'solutions.html'),
+                work: resolve(__dirname, 'work.html'),
+                contact: resolve(__dirname, 'contact.html'),
+            },
+        },
     },
 });
